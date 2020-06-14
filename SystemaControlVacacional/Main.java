234260ac3d6755d1e9766ef92e6c46052b9277a8
fernan2 <DeveloperBautista@gmail.com>
2020-06-13 21:18:02 -0400

    cambios significativos

--- SystemaControlVacacional/Main.java
+++ SystemaControlVacacional/Main.java
@@ -3,7 +3,6 @@ package interfaces;
 public class Main {
 
 	public static void main(String[] args) {
-
 		Procesos ps = new Procesos();
 
 		ps.inicio();
--- SystemaControlVacacional/Metodos.java
+++ SystemaControlVacacional/Metodos.java
@@ -1,46 +1,64 @@
 package interfaces;
 
+import javax.swing.JOptionPane;
+
 public class Metodos extends Propiedades {
 
 	public void opciones() {
 		if (getClave() == 1) {// Operario
 			if (getAntiguedad() == 1) {
-				System.out.println("El trabajador " + super.getNombre() + ", tiene derecho a 6 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 6 dias de vacaciones:");
 
 			} else if (getAntiguedad() >= 2 && getAntiguedad() <= 6) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 14 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 14 dias de vacaciones:");
 			} else if (getAntiguedad() == 7) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 20 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 20 dias de vacaciones:");
 			} else {
-				System.out.println("El trabajador  " + getNombre() + ", no tiene derecho a vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador  " + getNombre() + ", no tiene derecho a vacaciones:");
 			}
 
 		} else if (getClave() == 2) {
 			if (getAntiguedad() == 1) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 7 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 7 dias de vacaciones:");
 
 			} else if (getAntiguedad() >= 2 && getAntiguedad() <= 6) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 15 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 15 dias de vacaciones:");
 			} else if (getAntiguedad() == 7) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 22 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 22 dias de vacaciones:");
 			} else {
-				System.out.println("El trabajador  " + getNombre() + ", no tiene derecho a vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador  " + getNombre() + ", no tiene derecho a vacaciones:");
 			}
 
 		} else if (getClave() == 3) {// Gerente
 			if (getAntiguedad() == 1) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 10 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 10 dias de vacaciones:");
 
 			} else if (getAntiguedad() >= 2 && getAntiguedad() <= 6) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 20 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 20 dias de vacaciones:");
 			} else if (getAntiguedad() == 7) {
-				System.out.println("El trabajador  " + getNombre() + ", tiene derecho a 30 dias de vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador " + super.getNombre() + ", tiene derecho a 30 dias de vacaciones:");
 			} else {
-				System.out.println("El trabajador  " + getNombre() + ", no tiene derecho a vacaciones:");
+				JOptionPane.showMessageDialog(null,
+						"El trabajador  " + getNombre() + ", no tiene derecho a vacaciones:");
 			}
 		} else {
-			System.out.println("Error , clave no validad...");
+			JOptionPane.showMessageDialog(null, "Error, clave invalidad");
 		}
+	}
+
+	public Metodos() {
+
 	}
 
 }
